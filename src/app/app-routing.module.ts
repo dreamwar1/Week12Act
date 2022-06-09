09623918b006b25e33380e8255dac02ca4a4c810
dreamwar1 <sabinomichaelgarcia@gmail.com>
2022-06-09 19:53:10 +0800

    PUSH

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'loader',
+    loadChildren: () => import('./loader/loader.module').then( m => m.LoaderPageModule)
+  },
+  
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/loader/loader.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.page.html',
+  styleUrls: ['./loader.page.scss'],
+})
+export class LoaderPage implements OnInit {
+
+public email:any;
+public password:any;
+
+
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
